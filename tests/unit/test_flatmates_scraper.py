87585676324d81f8525c925ea40f85ff475bef0b
James Fricker <jimmyf98@hotmail.com>
2022-11-08 19:34:44 +1030

    enable cloudrun api (#8)
    * Update main.tf
    
    * fix tests

--- tests/unit/test_flatmates_scraper.py
+++ tests/unit/test_flatmates_scraper.py
@@ -1,14 +1,20 @@
 """simple tests for flatmates"""
 from src import flatmates_scraper
 
+
 def test_soup_from_url():
     """test if scraper returns data"""
     test_url = "https://flatmates.com.au/rooms/melbourne?page="
     get_flatmates_data = flatmates_scraper.soup_from_url(test_url)
     assert len(get_flatmates_data) > 0
 
+
 def test_get_flatmates_max_page():
     """test if max page is 220"""
-    assert 180 <= flatmates_scraper.get_flatmates_max_page(
-        "https://flatmates.com.au/rooms/melbourne") <= 250
-        
+    assert (
+        150
+        <= flatmates_scraper.get_flatmates_max_page(
+            "https://flatmates.com.au/rooms/melbourne"
+        )
+        <= 250
+    )
